6b5dc9ea76276ea45b59e4c35005c8a00354a4f4
arthurbugarin <54116812+arthurbugarin@users.noreply.github.com>
2019-12-01 16:57:34 -0300

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191201154200) do
+ActiveRecord::Schema.define(version: 20191201195308) do
 
   create_table "students", force: :cascade do |t|
     t.string "given_name"
@@ -21,6 +21,24 @@ ActiveRecord::Schema.define(version: 20191201154200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tools", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "quantity"
+    t.string "photo"
+    t.integer "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
