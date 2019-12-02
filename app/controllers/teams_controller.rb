8af5871e1335f28a136ebed8d76d1d681455fa95
arthurbugarin <54116812+arthurbugarin@users.noreply.github.com>
2019-12-01 22:20:54 -0300

    View de pesquisa
    Pesquisa adicionada às views de índice

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,7 +4,7 @@ class TeamsController < ApplicationController
   # GET /teams
   # GET /teams.json
   def index
-    @teams = Team.all
+    @teams = Team.search(params[:search])
   end
 
   # GET /teams/1
