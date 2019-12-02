fba0cdd9764d9617932c9f20c61492e2f50d6415
AngeloBot <angeloyanagita@gmail,com>
2019-12-02 14:57:55 -0300

    ajeitando uns rolê q seila o que é

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191202140329) do
+ActiveRecord::Schema.define(version: 20191202150717) do
 
   create_table "students", force: :cascade do |t|
     t.string "given_name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20191202140329) do
     t.string "password_digest"
     t.string "photo"
     t.boolean "admin"
+    t.string "team_id"
   end
 
   create_table "teams", force: :cascade do |t|
