ecac1a49c0c5f77a7df22e2fead3b95b3cfaf485
AngeloBot <angeloyanagita@gmail,com>
2019-12-03 15:59:40 -0300

    continuando_seeds
    está com erro ao puxar as imagens

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 #criando teams
-for i in ["AAA", "BBB", "CCC"]
+for i in ["AAA", "BBB", "CCC"] do
     @team=Team.create(name: i, description: "team "+ i +" AAA of applied mechatronics from Poli-USP")
     file= File.open(File.join(Rails.root, 'app/assets/images/logo_'+i+'.png'))
     image= @team.photo(filename: file)
@@ -17,7 +17,7 @@ end
 
 #criando team admins
 j=1
-for i in ["111", "333", "555"]
+for i in ["111", "333", "555"] do
     
     @student=Student.create(given_name: i, family_name: i, email: i, password: i, password_confirmation: i, team_id: j, admin: true)
     file= File.open(File.join(Rails.root, 'app/assets/images/foto_'+i+'.png'))
@@ -28,7 +28,7 @@ end
 
 #criando team members
 j=1
-for i in ["222","444","666"]
+for i in ["222","444","666"] do
     
     @student=Student.create(given_name: i, family_name: i, email: i, password: i, password_confirmation: i, team_id: j, admin: false)
     file= File.open(File.join(Rails.root, 'app/assets/images/foto_'+i+'.png'))
@@ -39,11 +39,15 @@ end
 
 #criando tools
 j=1
-for i in ["222","444","666"]
+t=1
+for i in ["furadeira", "serrote", "chave_de_fenda", "lixadeira_orbital", "martelo", "serra_arco"] do
     
-    @student=Student.create(given_name: i, family_name: i, email: i, password: i, password_confirmation: i, team_id: j, admin: false)
+    @tool=Student.create(name: i, description: i, email: i, quantity: j, team_id: t)
     file= File.open(File.join(Rails.root, 'app/assets/images/foto_'+i+'.png'))
-    image= @student.photo(filename: file)
+    image= @tool.photo(filename: file)
     image.save
     j=j+1
+    if j%2==0
+        t=t+1
+    end
 end
