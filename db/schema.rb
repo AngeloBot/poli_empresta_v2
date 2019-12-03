56e3e8298d74b5131b5e00743aadf6e6818d3423
Arthur Bugarin <arthur.mello.marques@gmail.com>
2019-12-03 12:03:25 -0300

    tentando resolver conflitos

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191202212252) do
-
-  create_table "loans", force: :cascade do |t|
-    t.boolean "accepted"
-    t.boolean "returned"
-    t.integer "borrower_id"
-    t.integer "owner_id"
-    t.integer "tool_quantity"
-    t.date "start"
-    t.date "end"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "tool_id"
-  end
+ActiveRecord::Schema.define(version: 20191202233000) do
 
   create_table "students", force: :cascade do |t|
     t.string "given_name"
@@ -35,7 +22,7 @@ ActiveRecord::Schema.define(version: 20191202212252) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "photo"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "team_id"
   end
 
