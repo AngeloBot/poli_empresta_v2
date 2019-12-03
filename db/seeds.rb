2b21940a1b6d8edd004ae90b8c9d425dabe43a82
AngeloBot <angeloyanagita@gmail,com>
2019-12-03 12:44:15 -0300

    preenchendo_seed.rb(incompleto)
    coloquei fotos em assets
    escrevendo rotina de seeds para teams, students e tools

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#criando teams
+for i in ["AAA", "BBB", "CCC"]
+    @team=Team.create(name: i, description: "team "+ i +" AAA of applied mechatronics from Poli-USP")
+    file= File.open(File.join(Rails.root, 'app/assets/images/logo_'+i+'.png'))
+    image= @team.photo(filename: file)
+    image.save
+end
+
+#criando team admins
+j=1
+for i in ["111", "333", "555"]
+    
+    @student=Student.create(given_name: i, family_name: i, email: i, password: i, password_confirmation: i, team_id: j, admin: true)
+    file= File.open(File.join(Rails.root, 'app/assets/images/foto_'+i+'.png'))
+    image= @student.photo(filename: file)
+    image.save
+    j=j+1
+end
+
+#criando team members
+j=1
+for i in ["222","444","666"]
+    
+    @student=Student.create(given_name: i, family_name: i, email: i, password: i, password_confirmation: i, team_id: j, admin: false)
+    file= File.open(File.join(Rails.root, 'app/assets/images/foto_'+i+'.png'))
+    image= @student.photo(filename: file)
+    image.save
+    j=j+1
+end
+
+#criando tools
+j=1
+for i in ["222","444","666"]
+    
+    @student=Student.create(given_name: i, family_name: i, email: i, password: i, password_confirmation: i, team_id: j, admin: false)
+    file= File.open(File.join(Rails.root, 'app/assets/images/foto_'+i+'.png'))
+    image= @student.photo(filename: file)
+    image.save
+    j=j+1
+end
